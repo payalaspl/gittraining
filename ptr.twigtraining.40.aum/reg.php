508576d448717c8585dba39ab475f86c82337046
payal <payal@aum.bz>
2020-05-26 18:18:26 +0530

    reg1

--- ptr.twigtraining.40.aum/reg.php
+++ ptr.twigtraining.40.aum/reg.php
@@ -30,4 +30,30 @@ $pattern3 = "/color/i";
 $text3 = "Color red is more visible than color blue in daylight.";
 $matches1 = preg_match_all($pattern3, $text3, $array);
 echo $matches1. " matches were found.";
+
+?><br/>
+<?php
+$subject='Give me 10 eggs';
+$pattern='~\b(\d+)\s*(\w+)$~';
+
+$success = preg_match($pattern, $subject, $match);
+if ($success) {
+	echo "Match: ".$match[0]."<br />"; 
+	echo "Group 1: ".$match[1]."<br />"; 
+	echo "Group 2: ".$match[2]."<br />"; 
+	}
+echo "<br>";
+$subject1='Give me 12 eggs then 12 more.';
+$pattern12='~\d+~';
+$newstring = preg_replace($pattern12, "6", $subject1);
+echo $newstring;
+
+$str = "token1@@ABC123token2@@DEF456token3";
+$regex = "/@@([A-Z]{3})(\d{3})/";
+print_r(preg_split($regex,$str,-1,PREG_SPLIT_DELIM_CAPTURE));
+
+
+$string2 = "(TheDayMyVoiceBroke)";
+$regex1 = "~(?=[A-Z])~";
+echo preg_replace($regex1," ",$string2);
 ?>
