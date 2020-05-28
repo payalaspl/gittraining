ee71d20b4a7f6352e4d8bb417cfdae0328e1dcc3
payal <payal@aum.bz>
2020-05-28 09:48:36 +0530

    custom tag

--- ptr.twigtraining.40.aum/bootstrap.php
+++ ptr.twigtraining.40.aum/bootstrap.php
@@ -2,10 +2,24 @@
 
 // Load our autoloader
 require_once __DIR__.'/vendor/autoload.php';
+require_once 'project_twig_extension.php';
 
 // Specify our Twig templates location
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 
+$filter = new \Twig\TwigFilter('rot13filter', function ($string) {
+    return str_rot13($string);
+});
+
+
+$function = new \Twig\TwigFunction('uppertext', function ($string) {
+    return strtoupper($string);
+});
+
+
 // Instantiate our Twig
 $twig = new \Twig\Environment($loader);
+$twig->addFilter($filter);
+$twig->addFunction($function);
+$twig->addExtension(new Project_Twig_Extension());
 ?>
---
+++ ptr.twigtraining.40.aum/project_twig_extension.php
@@ -0,0 +1,23 @@
+<?php
+
+class Project_Twig_Extension extends \Twig\Extension\AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+
+            
+
+             new \Twig\TwigFunction('lipsum', 'generate_lipsum'),
+        ];
+    }
+    public function getFilters()
+    {
+        return [
+            new \Twig\TwigFilter('rot13', 'str_rot13'),
+        ];
+    }
+
+}
+
+?>
